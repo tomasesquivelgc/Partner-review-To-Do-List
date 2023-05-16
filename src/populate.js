bd82f4952f4df1f719d72bbed214c16e78715ee3
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-16 12:53:20 -0300

    clone correct project

---
+++ src/populate.js
@@ -0,0 +1,108 @@
+import dragIcon from './images/drag_icon.svg';
+import deleteIcon from './images/delete.svg';
+import toggleCompleted from './complete-task.js';
+import deleteAllComplete from './modules/delete-complete-tasks.js';
+
+const list = document.getElementById('list');
+
+class TaskList extends Array {
+  constructor() {
+    super();
+    this.loadTasksFromLocalStorage();
+  }
+
+  render() {
+    list.innerHTML = '';
+    this.sort((a, b) => a.index - b.index);
+    for (let i = 0; i < this.length; i += 1) {
+      // create li element
+      const newLi = document.createElement('li');
+      // create neccesary items
+      // checkbox
+      const checkboxDescriptionDiv = document.createElement('div');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.classList.add('change');
+      if (this[i].completed === true) {
+        checkbox.checked = true;
+      }
+      // task description
+      const description = document.createElement('input');
+      description.type = 'text';
+      description.value = this[i].description;
+      description.classList.add('taskDescription');
+      // images
+      const dragIconImg = new Image();
+      dragIconImg.src = dragIcon;
+      const deleteIconImg = new Image();
+      deleteIconImg.src = deleteIcon;
+      deleteIconImg.classList.add('deleteBtnn');
+      deleteIconImg.setAttribute('data-id', i);
+      // checkboxDescription attach
+      checkboxDescriptionDiv.appendChild(checkbox);
+      checkboxDescriptionDiv.appendChild(description);
+      checkboxDescriptionDiv.classList.add('checkboxDescription');
+      // append the items to the li and provide functionality
+      newLi.appendChild(checkboxDescriptionDiv);
+      newLi.appendChild(deleteIconImg);
+      newLi.appendChild(dragIconImg);
+      list.appendChild(newLi);
+
+      deleteIconImg.addEventListener('click', () => {
+        this.removeTask(i);
+      });
+
+      description.addEventListener('change', (event) => {
+        const newDescription = event.target.value;
+        this[i].description = newDescription;
+        this.saveTasksToLocalStorage();
+      });
+
+      checkbox.addEventListener('change', () => {
+        this.toggleCompleted(i);
+        this.saveTasksToLocalStorage();
+      });
+    }
+    this.saveTasksToLocalStorage();
+  }
+
+  addTask(task) {
+    this.push(task);
+    this.render();
+  }
+
+  removeTask(position) {
+    this.splice(position, 1);
+    for (let i = 0; i < this.length; i += 1) {
+      this[i].index = i + 1;
+    }
+    this.render();
+  }
+
+  loadTasksFromLocalStorage() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+      tasks.forEach((task) => {
+        this.push(task);
+      });
+      this.render();
+    }
+  }
+
+  saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this));
+  }
+
+  toggleCompleted(index) {
+    const task = this[index];
+    toggleCompleted(task);
+    this.saveTasksToLocalStorage();
+  }
+
+  deleteCompletedTasks() {
+    deleteAllComplete(this);
+    this.render();
+  }
+}
+
+export default TaskList;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    Task: './src/Task.js',
+    populate: './src/populate.js',
+    clearAll: './src/modules/delete-complete-tasks.js',
+    checkComplete: './src/complete-task.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
