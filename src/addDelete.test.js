c416bf8e67f0e29f31281215a74fcc96920ac473
ibugithub <outsideworkibrahim@gmail.com>
2023-05-20 05:54:26 +0600

    Mock the Document object model

--- src/addDelete.test.js
+++ src/addDelete.test.js
@@ -12,8 +12,37 @@ const localStorageMock = {
 };
 global.localStorage = localStorageMock;
 
-describe('deleteAllCompleted', () => {
-  beforeEach(() => {
+// describe('addTask and removeTask', () => {
+//   // Before each test, clear the localStorage mock and set up the document body
+//   beforeEach(() => {
+//     // Test that the addTask method adds one <li> element to the list in the DOM
+
+//       // Create an instance of the class and call the addTask method
+//       addRemoveTask.addTask(mockEvent);
+
+//       // Get the list element from the document body
+//       const list = document.getElementById('taskList');
+
+//       // Assert that the list has one child element
+//       expect(list.children.length).toBe(1);
+
+//       // Assert that the child element is an <li> element with the expected content
+//       expect(list.children[0].tagName).toBe('LI');
+//       expect(list.children[0].textContent).toBe('New task');
+//     });
+//   });
+
+  describe('deleteAllCompleted', () => {
+    beforeEach(() => {
+    // Mock the dom
+      document.body.innerHTML = `
+    <div class="add_container">
+      <input id="inputElement" type="text" placeholder="Add to your list...">
+      <i class="fas fa-plus-circle"></i>
+    </div>
+    <ul id="taskList"></ul>
+  `;
+    });
     // Mock the localStorage data
     localStorage.getItem.mockReturnValue(JSON.stringify([{ id: 1, completed: false }]));
   });
